a1a77855bf8325d8f493ae4276fcfb531f743203
Muhamedzanov Elnur Eldarovich <elnurmuhamedzanov40@gmail.com>
2025-06-20 18:36:37 +0500

    Update add_missing_data.py

--- add_missing_data.py
+++ add_missing_data.py
@@ -4,7 +4,6 @@ from scipy.interpolate import interp1d
 
 
 def interpolate_bounding_boxes(data):
-    # Extract necessary data columns from input data
     frame_numbers = np.array([int(row['frame_nmr']) for row in data])
     car_ids = np.array([int(float(row['car_id'])) for row in data])
     car_bboxes = np.array([list(map(float, row['car_bbox'][1:-1].split())) for row in data])
@@ -17,7 +16,6 @@ def interpolate_bounding_boxes(data):
         frame_numbers_ = [p['frame_nmr'] for p in data if int(float(p['car_id'])) == int(float(car_id))]
         print(frame_numbers_, car_id)
 
-        # Filter data for a specific car ID
         car_mask = car_ids == car_id
         car_frame_numbers = frame_numbers[car_mask]
         car_bboxes_interpolated = []
@@ -77,15 +75,12 @@ def interpolate_bounding_boxes(data):
     return interpolated_data
 
 
-# Load the CSV file
 with open('test.csv', 'r') as file:
     reader = csv.DictReader(file)
     data = list(reader)
 
-# Interpolate missing data
 interpolated_data = interpolate_bounding_boxes(data)
 
-# Write updated data to a new CSV file
 header = ['frame_nmr', 'car_id', 'car_bbox', 'license_plate_bbox', 'license_plate_bbox_score', 'license_number', 'license_number_score']
 with open('test_interpolated.csv', 'w', newline='') as file:
     writer = csv.DictWriter(file, fieldnames=header)
